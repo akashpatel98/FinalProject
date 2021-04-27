aac5b613a6d1a791a6ea60f70e1237058a030009
akashpatel98 <abp78@njit.edu>
2021-04-26 20:25:35 -0400

    Task: Step 1

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, make_response, request, jsonify
+
+
+# Create Flask's `app` object
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    my_dict = {'key': 'dictionary value'}
+    headers = {"Content-Type": "application/json"}
+    return make_response(jsonify(my_dict), 200, headers)
