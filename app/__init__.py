d0ada7e5a502f8ed8519bf547a8da4c439cc1223
akashpatel98 <abp78@njit.edu>
2021-05-09 21:36:47 -0400

    Task: Step 11

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,42 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_session import Session
 
 
 # Globally accessible libraries
 db = SQLAlchemy()
-r = FlaskRedis()
-assets = Environment()
+login_manager = LoginManager()
+sess = Session()
 
 ddb = SQLAlchemy()
 login_manager = LoginManager()
+
+def create_app():
+    """Construct the core flask_session_tutorial."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    # Initialize Plugins
+    db.init_app(app)
+    login_manager.init_app(app)
+    sess.init_app(app)
+
+    with app.app_context():
+        from . import routes
+        from . import auth
+        from .assets import compile_static_assets, compile_auth_assets
+        app.register_blueprint(routes.main_bp)
+        app.register_blueprint(auth.auth_bp)
+
+        # Create static asset bundles
+        compile_static_assets(app)
+        compile_auth_assets(app)
+
+        # Create Database Models
+        db.create_all()
+
+        return app
 
 
 def init_app():
