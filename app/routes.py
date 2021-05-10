4b8704f4cad21bfe504acbdf46e2ca4bf283741d
akashpatel98 <abp78@njit.edu>
2021-05-09 20:27:28 -0400

    Task: Step 8

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from flask import current_app as app
 from flask import render_template
+from flask import Blueprint
 
+
+main_bp = Blueprint(
+    'main_bp',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
 
 @app.route('/')
 def home():
