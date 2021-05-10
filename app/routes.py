c99a1626c3a5c9f8d5f57cf9c145cc68299e0d39
akashpatel98 <abp78@njit.edu>
2021-05-09 21:16:34 -0400

    Task: Step 9

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import current_app as app
 from flask import render_template
 from flask import Blueprint
+from flask import request, make_response
+from datetime import datetime as dt
+from .models import db, User
 
 
 main_bp = Blueprint(
@@ -9,6 +12,55 @@ main_bp = Blueprint(
     template_folder='templates',
     static_folder='static'
 )
+
+@app.route('/', methods=['GET'])
+def user_records():
+    """Create a user via query string parameters."""
+    username = request.args.get('user')
+    email = request.args.get('email')
+    if username and email:
+        new_user = User(
+            username=username,
+            email=email,
+            created=dt.now(),
+            bio="In West Philadelphia born and raised, \
+            on the playground is where I spent most of my days",
+            admin=False
+        )
+        db.session.add(new_user)  # Adds new User record to database
+        db.session.commit()  # Commits all changes
+    return make_response(f"{new_user} successfully created!")
+
+@app.route('/', methods=['GET'])
+def user_records():
+    """Create a user via query string parameters."""
+    username = request.args.get('user')
+    email = request.args.get('email')
+    if username and email:
+        existing_user = User.query.filter(
+            User.username == username or User.email == email
+        ).first()
+        if existing_user:
+            return make_response(
+                f'{username} ({email}) already created!'
+            )
+        new_user = User(
+            username=username,
+            email=email,
+            created=dt.now(),
+            bio="In West Philadelphia born and raised, \
+            on the playground is where I spent most of my days",
+            admin=False
+        )  # Create an instance of the User class
+        db.session.add(new_user)  # Adds new User record to database
+        db.session.commit()  # Commits all changes
+        redirect(url_for('user_records'))
+    return render_template(
+        'users.jinja2',
+        users=User.query.all(),
+        title="Show Users"
+    )
+
 
 @app.route('/')
 def home():
